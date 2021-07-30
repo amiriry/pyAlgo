9af4886c4f8d0456d9f0681525340f3c1dbc7a8f
amirshkedy <amirshkedy@shkedy.co.il>
2021-07-30 20:03:51 +0300

    avl tree implementation

--- data_structures/avl_tree.py
+++ data_structures/avl_tree.py
@@ -3,8 +3,9 @@ Auto-balanced binary tree
 """
 
 import random
+import math
 
-class the_queue:
+class my_queue:
     def __init__(self):
         self.data = []
         self.head = 0
@@ -19,7 +20,7 @@ class the_queue:
 
     def pop(self):
         to_return = self.data[self.head]
-        self.tail = self.tail + 1
+        self.head = self.head + 1
         return to_return
 
     def count(self):
@@ -28,10 +29,10 @@ class the_queue:
     def print(self):
         print(self.data)
         print("*****************************")
-        print(self.data[self.head | self.tail])
+        print(self.data[self.head : self.tail])
 
 class my_node:
-    def __init__(self):
+    def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
@@ -76,30 +77,172 @@ def max_between(a,b):
     return b
 
 def right_rotation(node):
-    print("Node that is unbalanced:", node.get_data())
+    print("The node that is unbalanced:", node.get_data())
     to_return = node.get_left()
-    node.set_left(to_return.get_right())
+    node.set_left(to_return.get_right()) # The switch - the move to other side of the tree
     to_return.set_right(node)
-    h1 = my_max(get_height(node.get_right()), get_height(node.get_left())) + 1
+    h1 = max_between(get_height(node.get_right()), get_height(node.get_left())) + 1
     node.set_height(h1)
-    h2 = my_max(get_height(to_return.get_right()), get_height(to_return.get_left())) + 1
+    h2 = max_between(get_height(to_return.get_right()), get_height(to_return.get_left())) + 1
     to_return.set_height(h2)
-    return ret
+    return to_return
 
 def left_rotation(node):
+    print("right rotation node:", node.get_data())
+    to_return = node.get_right()
+    node.set_right(to_return.get_left())
+    to_return.set_left(node)
+    h1 = max_between(get_height(node.get_right()), get_height(node.get_left())) + 1
+    node.set_height(h1)
+    h2 = max_between(get_height(to_return.get_right()), get_height(to_return.get_left())) + 1
+    to_return.set_height(h2)
+    return to_return
+
 def left_right_rotation(node):
-def insert_node(node):
+    left_child = node.get_left()
+    node.set_left(left_rotation(left_child)) # The first rotation that keeps the tree not balanced
+    return right_rotation(node) # The rotation that balances the tree
+
+def right_left_rotation(node):
+    right_child = node.get_right()
+    node.set_right(right_rotation(right_child))
+    return left_rotation(node)
+
+def insert_node(add_node, data):
+    if add_node is None:
+        return my_node(data)
+    if data < add_node.get_data():
+        add_node.set_left(insert_node(add_node.get_left(), data))
+        if get_height(add_node.get_left()) - get_height(add_node.get_right()) == 2:
+            left_child = add_node.get_left()
+            if data < left_child.get_data():
+                add_node = right_rotation(add_node)
+            else:
+                add_node = left_right_rotation(add_node)
+    else: # data <= add_node.get_data()
+        add_node.set_right(insert_node(add_node.get_right(), data))
+        if get_height(add_node.get_right()) - get_height(add_node.get_left()) == 2:
+            right_child = add_node.get_right()
+            if data < right_child.get_data():
+                add_node = right_left_rotation(add_node)
+            else:
+                add_node = left_rotation(add_node)
+    h1 = max_between(get_height(add_node.get_right()), get_height(add_node.get_left())) + 1
+    add_node.set_height(h1)
+    return add_node
+
+def get_right_most(root):
+    while True:
+        right_child = root.get_right()
+        if right_child is None:
+            break
+        root = right_child
+    return root.get_data()
+
+def get_left_most(root):
+    while True:
+        left_child = root.get_left()
+        if left_child is None:
+            break
+        root = left_child
+    return root.get_data()
+
 def del_node(root, data):
+    left_child = root.get_left()
+    right_child = root.get_right()
+    if root.get_data() == data:
+        if left_child is not None and right_child is not None:
+            temp_data = get_left_most(right_child)
+            root.set_data(temp_data)
+            root.set_right(del_node(right_child, temp_data))
+        elif left_child is not None:
+            root = left_child
+        elif right_child is not None:
+            root = right_child
+        else:
+            return None
+    elif root.get_data() > data:
+        if left_child is None:
+            print("No such data")
+            return root
+        else:
+            root.set_left(del_node(left_child, data))
+    else: # root.get_data() < data
+        if right_child is None:
+            return root
+        else:
+            root.set_right(del_node(right_child, data))
+
+    if get_height(right_child) - get_height(left_child) == 2:
+        if get_height(right_child.get_right()) > get_height(right_child.get_left()):
+            root = left_rotation(root)
+        else:
+            root =right_left_rotation(root)
+    elif get_height(right_child) - get_height(left_child) == -2:
+        if get_height(left_child.get_left()) > get_height(left_child.get_right()):
+            root = right_rotation(root)
+        else:
+            root = left_right_rotation(root)
+    height = max_between(get_height(root.get_right()), get_height(root.get_left())) + 1
+    root.set_height(height)
+    return root
 
 
 class AVLTree:
     def __init__(self):
+        self.root = None
+
     def get_height(self):
+        return get_height(self.root)
+
     def insert(self, data):
+        print("insert:" + str(data))
+        self.root = insert_node(self.root, data)
+
     def del_node(self, data):
+        print("delete:" + str(data))
+        if self.root is None:
+            print("Treee is empty!")
+            return
+        self.root = del_node(self.root, data)
+
     def __str__(self):
+        output = ""
+        q = my_queue()
+        q.push(self.root)
+        layer = self.get_height()
+        if layer == 0:
+            return output
+        count = 0
+        while not q.is_empty():
+            node = q.pop()
+            space = " " * int(math.pow(2, layer - 1))
+            output += space
+            if node is None:
+                output += "*"
+                q.push(None)
+                q.push(None)
+            else:
+                output += str(node.get_data())
+                q.push(node.get_left())
+                q.push(node.get_right())
+            output += space
+            count += 1
+            for i in range(100):
+                if count == math.pow(2, i) - 1:
+                    layer = layer - 1
+                    if layer == 0:
+                        output += "\n***************************"
+                        return output
+                    output += "\n"
+                    break
+        output += "\n***************************"
+        return output
+
 
 def _test():
+    import doctest
+    doctest.testmod();
 
 if __name__ == "__main__":
     _test()
